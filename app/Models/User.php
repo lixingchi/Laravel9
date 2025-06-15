d49c0b950689bcda720e7e0242243e42950e3e69
lixingchi <994757127@qq.com>
2025-06-15 16:50:44 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,42 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // 我的粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 我的关注
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注用户
+    public function follow($user_ids)
+    {
+        if(! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if( ! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_id);
+    }
+
+    // 判断是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
